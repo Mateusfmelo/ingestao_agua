6c2f7cc72193e2ef793ebe9b6d98512f90ac9c41
Mateus Melo <62157751+Mateusfmelo@users.noreply.github.com>
2023-01-30 23:26:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function verificar() {
+
+    var cap = document.getElementById('peso')
+
+    if (cap.value <= 0) {
+        window.alert('Preencha o campo corretamente!')
+    } else {
+        peso = (cap.value * 0.35 / 10).toFixed(2).replace(".", ",")
+    }
+
+    res.innerHTML = `Você tem que ingerir um total de ${peso} litro(s) de água por dia!`
+}
