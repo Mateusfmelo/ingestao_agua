9302feeea4c3315d4fff8ec9253a22217b32b9e1
mateusfmelo <mateusmelo51@gmail.com>
2023-01-30 23:39:54 -0300

    Ajustes

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function verificar() {
 
     if (cap.value <= 0) {
         window.alert('Preencha o campo corretamente!')
+        res.innerHTML = ('Valor não aceito!')
     } else {
         peso = (cap.value * 0.35 / 10).toFixed(2).replace(".", ",")
+        res.innerHTML = `Você tem que ingerir um total de ${peso} litro(s) de água por dia!`
     }
-
-    res.innerHTML = `Você tem que ingerir um total de ${peso} litro(s) de água por dia!`
+    res.style.textAlign = 'center'
 }
